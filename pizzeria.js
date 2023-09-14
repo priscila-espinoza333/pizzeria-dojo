fb93f843b842d64b61b46dc9197550aee51e95e6
Pris <pr.espinoza@duocuc.cl>
2023-09-14 13:07:21 -0300

    final

--- pizzeria.js
+++ pizzeria.js
@@ -2,7 +2,7 @@ var pizza = {
     masa: ["madre", "tradicional", "delgada", "lanzada a mano"],
     tipoSalsa: ["salsa de tomate", "salsa blanca"],
     queso: ["feta", "queso mozzarella"],
-    verduras: ["tomate" ,"champiñon", "rucula", "aceitunas", "cebolla", "choclo"],
+    verduras: ["tomate" ,"champiñones", "rucula", "aceitunas", "cebollas", "choclo"],
     proteina: ["salame", "choricillo", "carne", "jamon", "salchicha"],
 };
 
@@ -19,8 +19,15 @@ function pizzaOven (masa, tipoSalsa, queso, verduras, proteina){
     return pizza;
 }
 
-var PizzEstiloChicago = ("tradicional", "mozzarella", "pepperoni", "salchicha");
+var PizzEstiloChicago = ("tradicional", "mozzarella", ["pepperoni", "salchicha"]);
 console.log(PizzEstiloChicago);
      
 
-var mariana = ("lanzada a mano", "ques ")
+var PizzaMariana = ("lanzada a mano", ["mozzarella, feta"], ["champiñones, aceitunas,cebollas"])
+console.log(PizzaMariana);
+
+var PizzaRoma = ("delgada", "mozzarella", ["salsa de tomate", "salsa blanca"] ,["champiñones, aceitunas"]);
+console.log(PizzaRoma);
+
+var PizzVegana = ("tradicional", "mozzarella", ["tomate" ,"champiñones", "rucula", "aceitunas", "cebollas", "choclo"]);
+console.log(PizzEstiloChicago);
